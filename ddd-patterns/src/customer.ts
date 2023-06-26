232739d106bca08a0401ca1f37b18eae309de3db
Fabio Marsiaj <fabiobackend@gmail.com>
2023-06-25 22:31:06 -0300

    Auto validation principle on domain

--- ddd-patterns/src/customer.ts
+++ ddd-patterns/src/customer.ts
@@ -4,22 +4,42 @@ class Customer {
   _id: string;
   _name: string;
   _address: string;
-  _active: boolean = true;
+  _active: boolean = false;
 
   constructor(id: string, name: string, address: string) {
     this._id = id;
     this._name = name;
     this._address = address;
+    this.validate();
   }
 
   // Validations should ensure that the actual state of the entity is correct.
   // We should not create validations or exceptions without meaning.
 
-  changeName(name: string) {
-    this._name = name;
+  // One entity should auto validate itself.
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+    if (this._address.length === 0) {
+      throw new Error("Address is required");
+    }
   }
 
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  // Expressing a specific bussiness logic.
+  // We should only activate a customer if the customer has an address.
   activate() {
+    if (this._address.length === 0) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
     this._active = true;
   }
   deactivate() {
